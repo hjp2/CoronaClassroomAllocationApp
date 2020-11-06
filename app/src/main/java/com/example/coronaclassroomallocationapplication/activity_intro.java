1d8e72b99c238e6684a34071620764071145e1fe
kojihun <chr6502@naver.com>
2020-11-06 23:50:26 +0900

    --------------모바일 프로그래밍-------------- 1. 메인 화면 버튼 이벤트 제작중 2. 인트로화면 구현 마무리

--- app/src/main/java/com/example/coronaclassroomallocationapplication/activity_intro.java
+++ app/src/main/java/com/example/coronaclassroomallocationapplication/activity_intro.java
@@ -8,14 +8,19 @@ import android.graphics.Point;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.Display;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class activity_intro extends AppCompatActivity {
     ImageView iv_intro;
+    ImageView iv_logo;
+    TextView tv_logo_name;
+    TextView tv_logo_desc;
+    Button bt_start;
 
     double screen_width, screen_height;
     float intro_width, intro_height;
-    float intro_center_x, intro_center_y;
 
     //인트로 화면입니다. 고지훈 수정중
     @Override
@@ -32,6 +37,7 @@ public class activity_intro extends AppCompatActivity {
         screen_width = size.x;
         screen_height = size.y;
 
+        //배경 이미지 가져오기
         iv_intro = new ImageView(this);
         iv_intro.setImageResource(R.drawable.intro_image);
         iv_intro.setScaleX(1.5f);
@@ -50,8 +56,33 @@ public class activity_intro extends AppCompatActivity {
         intro_translateX.start();
         intro_translateY.start();
 
+        //로고 이미지 가져오기
+        iv_logo = new ImageView(this);
+        iv_logo.setImageResource(R.drawable.intro_logo);
+        iv_logo.setScaleX(0.28f);
+        iv_logo.setScaleY(0.28f);
+        iv_logo.measure(iv_logo.getMeasuredWidth(), iv_logo.getMeasuredHeight());
+        layout.addView(iv_logo);
+
+        //로고 이름 및 로고 부연 설명 텍스트
+        tv_logo_name = new TextView(this);
+        tv_logo_desc = new TextView(this);
+        tv_logo_name.setText(R.string.logo_name);
+        tv_logo_desc.setText(R.string.logo_desc);
+        tv_logo_name.setTextSize(30f);
+
+        layout.addView(tv_logo_name);
+        layout.addView(tv_logo_desc);
 
 
+        //로고 위치 조정 코드
+        iv_logo.setY((float)screen_height - 3500f);
 
+        tv_logo_name.setX((float)screen_width - 930f);
+        tv_logo_name.setY((float)screen_height - 1900f);
+        tv_logo_desc.setX((float)screen_width - 980f);
+        tv_logo_desc.setY((float)screen_height - 1800f);
+
+        System.out.println("와이드:"+ screen_width);
     }
 }
