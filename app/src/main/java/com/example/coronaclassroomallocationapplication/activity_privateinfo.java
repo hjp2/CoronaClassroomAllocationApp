10954fbb8df10c72ffbe560b65fdcf681bf3c59a
clang <clang>
2020-11-16 13:24:15 +0900

    개인 정보 조회 및 변경 액티비티와 관련 클래스 정의

---
+++ app/src/main/java/com/example/coronaclassroomallocationapplication/activity_privateinfo.java
@@ -0,0 +1,20 @@
+package com.example.coronaclassroomallocationapplication;
+
+import android.os.Bundle;
+import android.text.Layout;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
+
+public class activity_privateinfo extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privateinfo);
+
+       //Layout layout = (Layout)findViewById();
+        //Button butt = (Button)findViewById(R.)
+    }
+}
---
+++ app/src/main/java/com/example/coronaclassroomallocationapplication/activity_secondprivateinfo.java
@@ -0,0 +1,15 @@
+package com.example.coronaclassroomallocationapplication;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_secondprivateinfo extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privateinfo2);
+
+    }
+}
