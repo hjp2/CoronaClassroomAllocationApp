b2a11ae70650c8f8bcd65684b0f5afa6005f83a3
hjp2 <nujeeh2@gmail.com>
2020-12-03 22:18:04 +0900

    2020-12-03 Firebase에 저장된 데이터로 건물선택, 층선택, 강의실 선택까지 진행함

--- app/src/main/java/com/example/coronaclassroomallocationapplication/activity_selectclass.java
+++ app/src/main/java/com/example/coronaclassroomallocationapplication/activity_selectclass.java
@@ -1,18 +1,35 @@
 package com.example.coronaclassroomallocationapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class activity_selectclass extends AppCompatActivity {
 
     TextView title;
-    ListView listView;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mReference;
+    private ChildEventListener mChild;
+
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    List<Object> Array = new ArrayList<Object>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,26 +39,96 @@ public class activity_selectclass extends AppCompatActivity {
         listView=(ListView)findViewById(R.id.listview);
         title = (TextView)findViewById(R.id.TextView_title);
 
-        title.setText("정보공학관 9층");
 
 
-        ArrayList<String> arrayList = new ArrayList<>();
+        String building = getIntent().getStringExtra("building");
+        String floor = getIntent().getStringExtra("floor");
 
-        arrayList.add("정보901 \n멀티미디어 실습실");
-        arrayList.add("정보901 \n멀티미디어 실습실");
-        arrayList.add("정보901 \n멀티미디어 실습실");
-        arrayList.add("정보901 \n멀티미디어 실습실");
-        arrayList.add("정보901 \n멀티미디어 실습실");
-        arrayList.add("정보901 \n멀티미디어 실습실");
-        arrayList.add("정보901 \n멀티미디어 실습실");
-        arrayList.add("정보901 \n멀티미디어 실습실");
-        arrayList.add("정보901 \n멀티미디어 실습실");
-        arrayList.add("정보901 \n멀티미디어 실습실");
+        title.setText(building+" "+floor);
+        initDatabase();
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,arrayList);
-        listView.setAdapter(arrayAdapter);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, new ArrayList<String>());
+        listView.setAdapter(adapter);
+
+        mReference = mDatabase.getReference("classroom/"+building+"/"+floor); // 변경값을 확인할 child 이름
+        mReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                adapter.clear();
+                for (DataSnapshot messageData : dataSnapshot.getChildren()) {
+
+                    String msg2 = messageData.getKey().toString();
+                    Array.add(msg2);
+                    adapter.add(msg2);
+                }
+                adapter.notifyDataSetChanged();
+                listView.setSelection(adapter.getCount() - 1);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                //Toast.makeText(getApplicationContext(), (String) Array.get(arg2),
+                //        Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), activity_selecttime.class);
+                intent.addFlags (Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                //intent.putExtra("floor", (String) Array.get(arg2));
+
+                startActivity(intent);
+            }
+        });
 
 
 
+
+    }
+
+
+    private void initDatabase() {
+
+        mDatabase = FirebaseDatabase.getInstance();
+
+        mReference = mDatabase.getReference("log");
+        mReference.child("log").setValue("check");
+
+        mChild = new ChildEventListener() {
+
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        mReference.addChildEventListener(mChild);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mReference.removeEventListener(mChild);
     }
 }
