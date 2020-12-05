9e48bb7fe2ca8c373f29a4dc9c99a0191b813351
hjp2 <nujeeh2@gmail.com>
2020-12-05 19:40:18 +0900

    2020-12-05 예약토글 기능 완료!!

--- app/src/main/java/com/example/coronaclassroomallocationapplication/activity_selecttime.java
+++ app/src/main/java/com/example/coronaclassroomallocationapplication/activity_selecttime.java
@@ -1,41 +1,23 @@
 package com.example.coronaclassroomallocationapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.SetOptions;
-import com.google.gson.JsonParser;
 
-import org.json.JSONObject;
-
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class activity_selecttime extends AppCompatActivity {
@@ -46,6 +28,11 @@ public class activity_selecttime extends AppCompatActivity {
     private ListViewAdapter adapter;
     int i = 0;
     int timecount[] = new int[8];
+    String [] docid = new String[8];
+
+    String rtime;
+    String rname;
+    String userid = mAuth.getCurrentUser().getUid();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,10 +58,6 @@ public class activity_selecttime extends AppCompatActivity {
         // 리스트뷰 참조 및 Adapter달기
         listView = (ListView) findViewById(R.id.listview);
         listView.setAdapter(adapter);
-        final String times[] = new String[]{"09:00 ~ 10:00","10:00 ~ 11:00","11:00 ~ 11:00","12:00 ~ 13:00","13:00 ~ 14:00","14:00 ~ 15:00","15:00 ~ 16:00", "16:00 ~ 17:00"};
-
-
-
 
         // 첫 번째 아이템 추가.
         //adapter.addItem("08:00 ~ 09:00","13 / 15", "예약하기");
@@ -89,24 +72,32 @@ public class activity_selecttime extends AppCompatActivity {
                             adapter.clearItem(); //리스트가 갱신되는게 아니라 화면에 쌓이기 때문에 정리를 해주어야한다.
                             //ids.clear();
                             timecount = new int[8];
+                            String times[] = new String[]{"09:00 ~ 10:00","10:00 ~ 11:00","11:00 ~ 12:00","12:00 ~ 13:00","13:00 ~ 14:00","14:00 ~ 15:00","15:00 ~ 16:00", "16:00 ~ 17:00"};
+                            String[] checkname = {"예약하기", "예약하기","예약하기","예약하기","예약하기","예약하기","예약하기","예약하기"};
+                            //String [] docid = new String[8];
+
+
+
                             for (DocumentSnapshot snap : queryDocumentSnapshots.getDocuments()) {
                                 Map<String, Object> shot = snap.getData();
-                                String rtime = String.valueOf(shot.get("시간"));
-                                System.out.println(rtime);
 
-                                if(rtime.equals(times[0])) timecount[0]++;
-                                if(rtime.equals(times[1])) timecount[1]++;
-                                if(rtime.equals(times[2])) timecount[2]++;
-                                if(rtime.equals(times[3])) timecount[3]++;
-                                if(rtime.equals(times[4])) timecount[4]++;
-                                if(rtime.equals(times[5])) timecount[5]++;
-                                if(rtime.equals(times[6])) timecount[6]++;
-                                if(rtime.equals(times[7])) timecount[7]++;
+                                rtime = String.valueOf(shot.get("시간"));
+                                rname = String.valueOf(shot.get("예약자"));
+                                String id = snap.getId();
 
+                                for(int k = 0 ; k<times.length ;k++){
+                                    if(rtime.equals(times[k])){
+                                        timecount[k]++;
+                                        if(rname.equals(userid)){
+                                            checkname[k] = "예약완료";
+                                            docid[k] = id;
+                                        }
+                                    }
 
+                                }
                             }
                             for(int j = 0;j<times.length;j++){
-                                adapter.addItem(times[j], Integer.toString(timecount[j]) +" / "+ max, "예약하기");
+                                adapter.addItem(times[j], Integer.toString(timecount[j]) +" / "+ max, checkname[j]);
                                 System.out.println("test");
                                 System.out.println(timecount[j]);
 
@@ -119,28 +110,22 @@ public class activity_selecttime extends AppCompatActivity {
                     }
                 });
 
-
-
-
-
-
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-                //Toast.makeText(getApplicationContext(), adapter.getItem(arg2).getTime(),
-                //        Toast.LENGTH_SHORT).show();
-                adapter.getItem(arg2).setState("예약완료");
-
-
                 if(mAuth.getCurrentUser() != null){
                     String rId = mStore.collection("classroom/"+building+"/층/"+floor+"/강의실/"+sclass+"/예약").document().getId();
                     Map<String, Object> data = new HashMap<>();
-                    data.put("날짜",sdate);
-                    data.put("예약자", mAuth.getCurrentUser().getUid());
-                    data.put("시간",adapter.getItem(arg2).getTime());
-                    mStore.collection("classroom/"+building+"/층/"+floor+"/강의실/"+sclass+"/예약").document(rId).set(data, SetOptions.merge());
+                    if(adapter.getItem(arg2).getState().equals("예약하기")){
+                        data.put("날짜",sdate);
+                        data.put("예약자", mAuth.getCurrentUser().getUid());
+                        data.put("시간",adapter.getItem(arg2).getTime());
+                        mStore.collection("classroom/"+building+"/층/"+floor+"/강의실/"+sclass+"/예약").document(rId).set(data, SetOptions.merge());
+                    }
+                    else if(adapter.getItem(arg2).getState().equals("예약완료")){
+                        mStore.collection("classroom/"+building+"/층/"+floor+"/강의실/"+sclass+"/예약").document(docid[arg2]).delete();
+                    }
                 }
-
             }
         });
 
@@ -148,4 +133,3 @@ public class activity_selecttime extends AppCompatActivity {
     }
 
 }
-
